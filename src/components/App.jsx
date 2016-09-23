88bd23613bcdfe29293c1bb699b0dd0e17075462
rene-justin-dado <justin.dado@yahoo.com>
2016-09-23 14:45:37 +1200

    connected WelcomeHeader component to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import WelcomeHeader from './WelcomeHeader'
 
 export default React.createClass({
   render () {
     return (
       <div className='app-container'>
-        // components
+        <WelcomeHeader />
       </div>
     )
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
-import {combineReducers} from 'redux'
-
+// import {combineReducers} from 'redux'
+//
 // import reducers
-
-export default combineReducers({
-  // reducer name
-})
+//
+// export default combineReducers({
+//   // reducer name
+// })
