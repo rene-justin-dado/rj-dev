d5f520f11823c1018c9e43d2f66fd77a3fb248b1
Justin <justindado@yahoo.com>
2016-09-25 20:16:24 +1300

    added BlogsExcerpt component

---
+++ src/components/BlogsExcerpt.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass ({
+  render () {
+    return (
+      <section>
+        <img src="" alt="" />
+        <a href="#BlogsPageLink">Blogs</a>
+        <p>Check out my blogs, where I talk about..... stuff!</p>
+      </section>
+    )
+  }
+})
