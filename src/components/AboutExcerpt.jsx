176337d98d7106074ef0aed437ab2b5acb3d2a1d
Justin <justindado@yahoo.com>
2016-09-25 20:10:23 +1300

    added AboutExcerpt component

---
+++ src/components/AboutExcerpt.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass ({
+  render () {
+    return (
+      <section>
+        <img src="" />
+        <a href="#AboutPageLink">About</a>
+        <p>This is some info about me!</p>
+      </section>
+    )
+  }
+})
