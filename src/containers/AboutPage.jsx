de21a3aac7a4563e545f936740ad0c831f1420cc
amandine-perichon <apericho@gmail.com>
2016-09-27 16:15:27 +1300

    initialised simple AboutPage container for about path

---
+++ src/containers/AboutPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default React.createClass ({
+  render () {
+    return (
+      <div className="about-page">
+        <h3>I love <3</h3>
+        <ul>
+          <li>Tech and...</li>
+          <li>Good Stories and...</li>
+          <li>Good Company and...</li>
+          <li>Sugar and...</li>
+          <li>That melancholy feeling you get when you reminisce about past mistakes and realise that you've changed a lot since then.</li>
+        </ul>
+      </div>
+    )
+  }
+})
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import {render} from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 
 import App from './components/App'
+import AboutPage from './components/AboutPage'
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
     <Router history={hashHistory}>
       <Route path="/" component={App} />
+      <Route path="/about" component={AboutPage} />
     </Router>,
     document.getElementById('app')
   )
