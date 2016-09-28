f953089759790a59e8dc1d80fb51ddbd219f89e0
Justin <justindado@yahoo.com>
2016-09-28 19:14:20 +1300

    changed header placeholders

--- src/components/DropDownNav.jsx
+++ src/components/DropDownNav.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default React.createClass ({
   render () {
     return (
-      <a className="drop-down-nav" href="#">menu</a>
+      <a className="drop-down-nav" href="#">future drop-down-nav</a>
     )
   }
 })
