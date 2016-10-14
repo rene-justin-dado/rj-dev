2bb6c25bf831462edfd8dc3e43e4c85845de359a
Justin <justindado@yahoo.com>
2016-10-14 16:02:01 +1300

    added actions to get projects data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,23 @@
-// Add actions here as needed
+import request from 'superagent'
+
+const url = 'http://localhost:3000'
+
+export const receiveProjects = (projects) => {
+  return {
+    type: 'RECEIVE_PROJECTS',
+    projects: projects.map(project => project.data)
+  }
+}
+
+export function getAllProjects () {
+  return () => {
+    return request
+      .get(`https://crossorigin.me/${url}/v1/projects`)
+      .end((err, res) => {
+        if (err) {
+          res.send('Whoops! There was a problem!')
+        }
+        dispatch(receiveProjects())
+      })
+  }
+}
