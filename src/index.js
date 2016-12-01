eb4757b69bfc290109102faca984755fb8977d7e
= <justindado@yahoo.com>
2016-12-02 11:37:29 +1300

    set up redux dev tools

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import BlogsLandingPage from './components/BlogsLandingPage'
 import CulturalBlogs from './components/CulturalBlogs'
 import AboutPage from './components/AboutPage'
 
-const store = createStore(reducers)
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
