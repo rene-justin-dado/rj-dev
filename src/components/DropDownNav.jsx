028ee28bbc8b7d9800aade024c7d039133e34e9d
Justin <justindado@yahoo.com>
2016-12-06 17:59:32 +1300

    added dropdown nav

--- src/components/DropDownNav.jsx
+++ src/components/DropDownNav.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 export default React.createClass({
   render () {
     return (
-      <a className="drop-down-nav" href="#">future drop-down-nav</a>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="dropdown">
+            <a className="dropdown-toggle" data-toggle="dropdown">Navigate</a>
+            <ul className="dropdown-menu">
+              <li><a href="#">Link 1</a></li>
+              <li><a href="#">Link 2</a></li>
+              <li><a href="#">Link 3</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
   }
 })
