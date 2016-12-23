3cdfe73dab5857105259acf6664e26ab0d43f192
Justin <justindado@yahoo.com>
2016-12-23 22:00:02 +1300

    basic rendering of blog content from JSON

--- src/components/CulturalBlogs.jsx
+++ src/components/CulturalBlogs.jsx
@@ -25,11 +25,14 @@ export default React.createClass({
   },
   render () {
     const blogs = this.state.blogs.map((elem, i) => {
+      const content = JSON.parse(elem.content)
       return (
         <div key={i} className="blog-info">
           <h5>{elem.category}</h5>
           <h4>{elem.title}</h4>
-          {elem.content}
+          <h4>{content.title}</h4>
+          <h5>{content.questions}</h5>
+          <p>{content.answers}</p>
         </div>
       )
     })
