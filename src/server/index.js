0e209eb45cb924fa8dbc3626da5847de7619f123
Justin <justindado@yahoo.com>
2016-12-23 22:00:34 +1300

    prioritising localhost for server port

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
+const PORT = 5000 || process.env.PORT
 const app = express()
-const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + './../public'))
 
