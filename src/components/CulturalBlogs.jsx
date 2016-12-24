31b42c142ad2d0c945f3fc408114ecdfae060df3
Justin <justindado@yahoo.com>
2016-12-24 20:40:39 +1300

    prioritising deploy

--- src/components/CulturalBlogs.jsx
+++ src/components/CulturalBlogs.jsx
@@ -3,7 +3,7 @@ import request from 'superagent'
 import {Link} from 'react-router'
 
 const proxy = 'https://crossorigin.me/',
-      url = 'http://localhost:3000' || `${proxy}rj-dev-backend.herokuapp.com`
+      url = `${proxy}rj-dev-backend.herokuapp.com` || 'http://localhost:3000'
 
 export default React.createClass({
   getInitialState () {
