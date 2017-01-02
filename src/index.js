e690689dc4dc07b670fed686fb2596eb23ed427b
Justin <justindado@yahoo.com>
2017-01-02 20:47:48 +1300

    added compose for redux middleware and devtools

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {render} from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
 import {Provider} from 'react-redux'
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 
 import rootReducer from './reducers'
@@ -14,8 +14,10 @@ import AboutPage from './components/AboutPage'
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 document.addEventListener('DOMContentLoaded', () => {
