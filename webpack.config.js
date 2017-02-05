c1bc5be44c3c4663a52d3ff35663ff3e477ff967
Justin <justindado@yahoo.com>
2017-02-05 21:30:02 +1300

    update webpack for sass loading (not yet working with file output)

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
-    path: path.join(__dirname, 'src', 'public'),
+    path: path.resolve(__dirname, 'src', 'public'),
     filename: 'bundle.js'
   },
   module: {
     loaders: [
+      // Babel Loader
       {
         loader: 'babel-loader?cacheDirectory',
         test: /\.jsx?$/,
         exclude: 'node_modules'
+      },
+      // Sass Loader
+      {
+        loader: ExtractTextPlugin.extract('css-loader?sourceMap', 'sass-loader?sourceMap'),
+        test: /\.scss$/
       }
     ]
   },
+  sassLoader: {
+    file: path.resolve(__dirname, 'src', 'public', 'styles', 'main.scss'),
+    includePaths: [path.resolve(__dirname, 'src', 'public', 'styles')],
+    outFile: path.resolve(__dirname, 'src', 'public', 'styles', 'main.css'),
+    outputStyle: 'compressed'
+  },
+  // For outputting css file during bundling
+  plugins: [
+    new ExtractTextPlugin('public/styles/main.css', {
+      allChunks: true
+    })
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
