7bd73eccbc9901b70bdeb8490b42cd4b6392aa28
Justin <justindado@yahoo.com>
2017-02-05 18:34:23 +1300

    updated static with a relative path

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const cfg = require('../config')
+const path = require('path')
 const PORT = cfg.port
 const app = express()
 
-app.use(express.static(__dirname + './../public'))
+app.use(express.static(path.resolve(__dirname, 'src', 'public')))
 
 app.listen(PORT, function () {
   console.log('Listening on port ' + PORT)
