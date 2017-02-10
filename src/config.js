50f2d1f9e9ef4dffa8aea5a09adf333451757ab6
Justin <justindado@yahoo.com>
2017-02-10 16:09:55 +1300

    updated config with production url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,9 @@ const development = {
   port: 5000,
   url: 'http://localhost:3000'
 }
-const production = { url: process.env.PORT }
+const production = {
+  port: process.env.PORT,
+  url: 'http://rj-dev.herokuapp.com/'
+}
 
 module.exports = process.env.NODE_ENV === 'production' ? production : development
