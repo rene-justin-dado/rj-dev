aa7781b12c69c478e7432b52b37316e295f95e76
Justin <justindado@yahoo.com>
2017-02-10 21:00:01 +1300

    added nav header

--- src/components/BlogEntryForm.jsx
+++ src/components/BlogEntryForm.jsx
@@ -6,27 +6,37 @@ const url = config.url
 export default React.createClass({
   render () {
     return (
-      <form id="blog_entry_form"
-            action={`${url}/v1/blogs/add/by/:category/post`}
-            method="post">
-        <label className="blog_entry">Category</label>
-        <input type="text"
-               name="category"/>
-        <label className="blog_entry">Title</label>
-        <input type="text"
-               name="title"/>
-        <label className="blog_entry">Sub-Title</label>
-        <input type="text"
-               name="sub_title"/>
-        <label className="blog_entry">Content</label>
-        <textarea name='content'
-                  rows='10'
-                  cols='50'/>
-        <button name="add_blog_entry"
-                value="add_blog_entry">
-          Add Entry
-        </button>
-      </form>
+      <div className="blog-entry-form">
+        <header>
+          <Link to="/">
+            <h1>Home</h1>
+          </Link>
+          <Link to="/blogs">
+            <h1>Blogs Landing</h1>
+          </Link>
+        </header>
+        <form id="blog_entry_form"
+              action={`${url}/v1/blogs/add/by/:category/post`}
+              method="post">
+          <label className="blog_entry">Category</label>
+          <input type="text"
+                 name="category"/>
+          <label className="blog_entry">Title</label>
+          <input type="text"
+                 name="title"/>
+          <label className="blog_entry">Sub-Title</label>
+          <input type="text"
+                 name="sub_title"/>
+          <label className="blog_entry">Content</label>
+          <textarea name='content'
+                    rows='10'
+                    cols='50'/>
+          <button name="add_blog_entry"
+                  value="add_blog_entry">
+            Add Entry
+          </button>
+        </form>
+      </div>
     )
   }
 })
