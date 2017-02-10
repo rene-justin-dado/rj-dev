efae151611b4cef70a992adac769b3e779b37add
Justin <justindado@yahoo.com>
2017-02-10 15:49:48 +1300

    updated blog entry form with input names

--- src/components/BlogEntryForm.jsx
+++ src/components/BlogEntryForm.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
+import config from '../config'
+
+const url = config.url
 
 export default React.createClass({
   render () {
     return (
-      <form id="blog_entry_form" action="/add/post/route">
+      <form id="blog_entry_form"
+            action={`${url}/v1/blogs/add/by/:category/post`}
+            method="post">
         <label className="blog_entry">Category</label>
-        <input type="text"/>
+        <input type="text"
+               name="category"/>
         <label className="blog_entry">Title</label>
-        <input type="text"/>
+        <input type="text"
+               name="title"/>
         <label className="blog_entry">Sub-Title</label>
-        <input type="text"/>
+        <input type="text"
+               name="sub_title"/>
         <label className="blog_entry">Content</label>
-        <textarea name='content_text_area'
+        <textarea name='content'
                   rows='10'
                   cols='50'/>
         <button name="add_blog_entry"
-                formMethod="post"
                 value="add_blog_entry">
           Add Entry
         </button>
