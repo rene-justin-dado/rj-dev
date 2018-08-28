5ebf0a2fbd8ca26bac3114dc9ff39553fbeb425b
Justin Dado <justindado@yahoo.com>
2018-08-29 11:31:02 +1200

    Removed Redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Router, Route, hashHistory} from 'react-router'
-import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
 
-import rootReducer from './reducers'
 import App from './components/App'
 import ProjectsLandingPage from './components/ProjectsLandingPage'
 import BlogsContainer from './containers/BlogsContainer'
@@ -13,26 +9,16 @@ import BlogEntryForm from './components/BlogEntryForm'
 import AboutPage from './components/AboutPage'
 import CategoryBlog from './components/CategoryBlog'
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
-
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <Provider store={store}>
-      <Router history={hashHistory}>
-        <Route path="/" component={App} />
-        <Route path="/projects" component={ProjectsLandingPage} />
-        <Route path="/blogs" component={BlogsContainer} />
-        <Route path="/blogs/by/:category" component={CategoryBlog} />
-        <Route path="/blogs/add/by/:category" component={BlogEntryForm} />
-        <Route path="/about" component={AboutPage} />
-      </Router>
-    </Provider>,
+    <Router history={hashHistory}>
+      <Route path="/" component={App} />
+      <Route path="/projects" component={ProjectsLandingPage} />
+      <Route path="/blogs" component={BlogsContainer} />
+      <Route path="/blogs/by/:category" component={CategoryBlog} />
+      <Route path="/blogs/add/by/:category" component={BlogEntryForm} />
+      <Route path="/about" component={AboutPage} />
+    </Router>,
     document.getElementById('app')
   )
 })
